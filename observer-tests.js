1718153a3d7c4eeb6bbb5d4ce9c705da58c9bdde
Duncan Cragg <fjord@cilux.org>
2010-01-25 21:45:11 +0000

    triple observer ping pong

--- observer-tests.js
+++ observer-tests.js
@@ -14,15 +14,15 @@ sys.puts('------------------ Fjord Tests ---------------------');
 
 // -------------------------------------------------------------------
 
-var r11 = WebObject.create('{ "tags": "one", "state": "/11/12/", "%refs": { "tags": "two", "state": "22" } }');
-var r12 = WebObject.create('{ "tags": "one", "state": "/12/13/", "%refs": { "tags": "two", "state": "23" } }');
+r11 = WebObject.create('{ "tags": "one", "state": "/11/12/", "%refs": { "tags": "two", "state": "22" } }');
+r12 = WebObject.create('{ "tags": "one", "state": "/12/13/", "%refs": { "tags": "two", "state": "23" } }');
 
-var r21 = WebObject.create('{ "tags": "two", "state": "/21/22/", "o1": { "state": "11" } }');
-var r22 = WebObject.create('{ "tags": "two", "state": "/22/23/", "o1": { "state": "12" } }');
+r21 = WebObject.create('{ "tags": "two", "state": "/21/22/", "o1": { "state": "11" } }');
+r22 = WebObject.create('{ "tags": "two", "state": "/22/23/", "o1": { "state": "12" } }');
 
 
-var o1  = WebObject.create('{ "tags": "one", "state": "11" }',                 [ r11, r12 ] );
-var o2  = WebObject.create('{ "tags": "two", "state": "21", "o1": "'+o1+'" }', [ r21, r22 ] );
+o1  = WebObject.create('{ "tags": "one", "state": "11" }',                 [ r11, r12 ] );
+o2  = WebObject.create('{ "tags": "two", "state": "21", "o1": "'+o1+'" }', [ r21, r22 ] );
 
 
 test.objectsEqual("Double Observer ping-pong worked on o1",
@@ -51,6 +51,39 @@ test.objectsEqual("Etag starts at 0, increments only on change",
                    new WebObject('{ "a": [ "0", "1", "2" ] }'));
 
 log("obj=", Cache[obj]);
+
+// -------------------------------------------------------------------
+
+r11 = WebObject.create('{ "tags": "one", "state": "/11/12/", "%refs": [ { "state": "22" }, { "state": "32" } ] }');
+r12 = WebObject.create('{ "tags": "one", "state": "/12/13/", "%refs": [ { "state": "23" }, { "state": "33" } ] }');
+r13 = WebObject.create('{ "tags": "one", "state": "/13/14/", "%refs": [ { "state": "24" }, { "state": "34" } ] }');
+
+r21 = WebObject.create('{ "tags": "two", "state": "/21/22/", "o1": { "state": "11" } }');
+r22 = WebObject.create('{ "tags": "two", "state": "/22/23/", "o1": { "state": "12" } }');
+r23 = WebObject.create('{ "tags": "two", "state": "/23/24/", "o1": { "state": "13" } }');
+
+r31 = WebObject.create('{ "tags": "thr", "state": "/31/32/", "o1": { "state": "11" } }');
+r32 = WebObject.create('{ "tags": "thr", "state": "/32/33/", "o1": { "state": "12" } }');
+r33 = WebObject.create('{ "tags": "thr", "state": "/33/34/", "o1": { "state": "13" } }');
+
+
+o1  = WebObject.create('{ "tags": "one", "state": "11" }',                 [ r11, r12, r13 ] );
+o2  = WebObject.create('{ "tags": "two", "state": "21", "o1": "'+o1+'" }', [ r21, r22, r23 ] );
+o3  = WebObject.create('{ "tags": "thr", "state": "31", "o1": "'+o1+'" }', [ r31, r32, r33 ] );
+
+
+test.objectsEqual("Triple Observer ping-pong worked on o1",
+                   Cache[o1], new WebObject('{ "tags": "one", "state":"14" }'));
+
+test.objectsEqual("Triple Observer ping-pong worked on o2",
+                   Cache[o2], new WebObject('{ "tags": "two", "state":"24", "o1":"'+o1+'" }'));
+
+test.objectsEqual("Triple Observer ping-pong worked on o3",
+                   Cache[o3], new WebObject('{ "tags": "thr", "state":"34", "o1":"'+o1+'" }'));
+
+test.isTrue("Etag of o1 now 3", Cache[o1].etag===3);
+test.isTrue("Etag of o2 now 3", Cache[o2].etag===3);
+test.isTrue("Etag of o3 now 3", Cache[o3].etag===3);
 
 // -------------------------------------------------------------------
 
